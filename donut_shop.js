ad76b6faf924e856673bb8e9e732a1ac064a0884
Colin Coltrera <c.coltrera@gmail.com>
2015-05-18 16:56:03 -0700

    Make creating or updating shops a method of shops object.

--- donut_shop.js
+++ donut_shop.js
@@ -69,6 +69,25 @@
          }
        }
     },
+    createUpdateShop: function(e) {
+      var location, minCustomers, maxCustomers, avgPurchase;
+      e.preventDefault();
+      location = document.getElementById("new_shop_location").value;
+      minCustomers = parseInt(document.getElementById("new_shop_min_customers").value);
+      maxCustomers = parseInt(document.getElementById("new_shop_max_customers").value);
+      avgPurchase = parseInt(document.getElementById("new_shop_avg_purchase").value);
+      if (location.length > 0 && !isNaN(minCustomers) && minCustomers >= 0 && !isNaN(maxCustomers) && maxCustomers >= 0 && maxCustomers > minCustomers && !isNaN(avgPurchase) && avgPurchase > 0) {
+        if (shops[location.toLowerCase()] == undefined) {
+          shops[location.toLowerCase()] = new Shop(location, minCustomers, maxCustomers, avgPurchase);
+        }
+        else {
+          shops[location.toLowerCase()].minCustomers = parseInt(minCustomers);
+          shops[location.toLowerCase()].maxCustomers = parseInt(maxCustomers);
+          shops[location.toLowerCase()].avgPurchase = parseInt(avgPurchase);
+        }
+        shops[location.toLowerCase()].writeToTable();
+      }
+    },
     downtown: new Shop("Downtown", 8, 43, 4.50),
     capitolHill: new Shop("Capitol Hill", 4, 37, 2.00),
     southLakeUnion: new Shop("South Lake Union", 9, 23, 6.33),
@@ -76,28 +95,8 @@
     ballard: new Shop("Ballard", 8, 58, 3.75)
   }
 
+  document.getElementById("new_shop_button").addEventListener("click", shops.createUpdateShop);
+
   shops.writeAllShops();
-
-  document.getElementById("new_shop_button").addEventListener("click", function(e) {
-    var location, minCustomers, maxCustomers, avgPurchase;
-    e.preventDefault();
-    location = document.getElementById("new_shop_location").value;
-    minCustomers = parseInt(document.getElementById("new_shop_min_customers").value);
-    maxCustomers = parseInt(document.getElementById("new_shop_max_customers").value);
-    avgPurchase = parseInt(document.getElementById("new_shop_avg_purchase").value);
-    if (location.length > 0 && !isNaN(minCustomers) && minCustomers >= 0 && !isNaN(maxCustomers) && maxCustomers >= 0 && maxCustomers > minCustomers && !isNaN(avgPurchase) && avgPurchase > 0) {
-      if (shops[location.toLowerCase()] == undefined) {
-        shops[location.toLowerCase()] = new Shop(location, minCustomers, maxCustomers, avgPurchase);
-      }
-      else {
-        shops[location.toLowerCase()].minCustomers = parseInt(minCustomers);
-        shops[location.toLowerCase()].maxCustomers = parseInt(maxCustomers);
-        shops[location.toLowerCase()].avgPurchase = parseInt(avgPurchase);
-      }
-      shops[location.toLowerCase()].writeToTable();
-      console.table(shops);
-    }
-  });
-
 
 }());
